1a018764b49d6a4aec44d865a481f810e56372a0
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-09-03 21:44:22 -0300

    Gardening functions 2 exercise :seedling:

--- chapter-5/functions-2.exs
+++ chapter-5/functions-2.exs
@@ -1,11 +1,11 @@
-fizzbuzz = fn
+fizzbuzz_sentence = fn
   0, 0, _ -> "FizzBuzz"
   0, _, _ -> "Fizz"
   _, 0, _ -> "Buzz"
   _, _, c -> c
 end
 
-IO.puts "With zero for all args: #{fizzbuzz.(0, 0, 0)}"
-IO.puts "With zero for first arg: #{fizzbuzz.(0, 1, 2)}"
-IO.puts "With zero for second arg: #{fizzbuzz.(1, 0, 2)}"
-IO.puts "With zero for second arg: #{fizzbuzz.(1, 2, 3)}"
+IO.puts "With zero for all args: #{fizzbuzz_sentence.(0, 0, 0)}"
+IO.puts "With zero for first arg: #{fizzbuzz_sentence.(0, 1, 2)}"
+IO.puts "With zero for second arg: #{fizzbuzz_sentence.(1, 0, 2)}"
+IO.puts "With zero for second arg: #{fizzbuzz_sentence.(1, 2, 3)}"
