ce11c9d6c7e7c7dd90238ee20952dc3c19d6dedc
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-09-09 13:53:49 -0300

    Solve lists and recursion exercise no. 1

---
+++ chapter-7/lists_and_recursion_1.ex
@@ -0,0 +1,8 @@
+defmodule MyList do
+  def mapsum([], _), do: 0
+  def mapsum([head | tail], func) do
+    func.(head) + mapsum(tail, func)
+  end
+end
+
+IO.puts MyList.mapsum [1, 2, 3], &(&1 * &1)
