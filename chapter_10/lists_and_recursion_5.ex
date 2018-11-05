2f5afd8a55eea273b6b91b08b91907f78fdb9ab9
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-11-05 00:06:24 -0200

    Implement MyEnum.each

--- chapter_10/lists_and_recursion_5.ex
+++ chapter_10/lists_and_recursion_5.ex
@@ -10,11 +10,23 @@ defmodule MyEnum do
     end
   end
 
-  # TODOs: each, filter, split and take
+  def each([], _) do
+    :ok
+  end
+  def each([head | tail], fun) do
+    fun.(head)
+
+    each(tail, fun)
+  end
+  # TODOs: filter, split and take
 end
 
 even_fun = fn x ->
   rem(x, 2) == 0
+end
+
+puts_fun = fn x ->
+  IO.puts "--> Inside fun: #{x}"
 end
 
 IO.puts "MyEnum.all?"
@@ -23,4 +35,12 @@ IO.inspect MyEnum.all?([], even_fun)
 IO.inspect MyEnum.all?([1], even_fun)
 IO.inspect MyEnum.all?([2, 4, 6], even_fun)
 IO.inspect MyEnum.all?([2, 3, 4], even_fun)
+IO.puts "---\n"
+
+IO.puts "MyEnum.each"
+IO.puts "---"
+IO.inspect MyEnum.each([], puts_fun)
+IO.inspect MyEnum.each([1], puts_fun)
+IO.inspect MyEnum.each([2, 4, 6], puts_fun)
+IO.inspect MyEnum.each([2, 3, 4], puts_fun)
 IO.puts "---\n"
