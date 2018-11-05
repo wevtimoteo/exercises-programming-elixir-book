531bed3950c3a00027c4250cae2377c8c00cf512
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-11-05 00:06:13 -0200

    Use even_fun instead repeating it

--- chapter_10/lists_and_recursion_5.ex
+++ chapter_10/lists_and_recursion_5.ex
@@ -21,6 +21,6 @@ IO.puts "MyEnum.all?"
 IO.puts "---"
 IO.inspect MyEnum.all?([], even_fun)
 IO.inspect MyEnum.all?([1], even_fun)
-IO.inspect MyEnum.all?([2, 4, 6], fn x -> rem(x, 2) == 0 end)
-IO.inspect MyEnum.all?([2, 3, 4], fn x -> rem(x, 2) == 0 end)
+IO.inspect MyEnum.all?([2, 4, 6], even_fun)
+IO.inspect MyEnum.all?([2, 3, 4], even_fun)
 IO.puts "---\n"
