8d31e340d6e0615d671ff419513a9184c06f3b95
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-11-14 00:01:16 -0200

    Implement MyEnum.split

--- chapter_10/lists_and_recursion_5.ex
+++ chapter_10/lists_and_recursion_5.ex
@@ -40,7 +40,33 @@ defmodule MyEnum do
       _filter(tail, fun, acc ++ [head])
     end
   end
-  # TODOs: split and take
+
+  def split([], _) do
+    []
+  end
+  def split([element], count) do
+    {[element], []}
+  end
+  def split(enumerable, count) when count < 0 do
+    reverse_count = count * -1
+
+    if reverse_count >= length(enumerable) do
+      {[], enumerable}
+    else
+      split(enumerable, length(enumerable) - reverse_count)
+    end
+  end
+  def split([head | tail], count) do
+    _split([head], tail, count - 1)
+  end
+
+  defp _split(enumerable, [head | tail], count) when count > 0 do
+    _split(enumerable ++ [head], tail, count - 1)
+  end
+  defp _split(split_enum, enumerable, count) when count == 0 do
+    {split_enum, enumerable}
+  end
+  # TODOs: take
 end
 
 even_fun = fn x ->
@@ -73,4 +99,13 @@ IO.inspect MyEnum.filter([], even_fun)
 IO.inspect MyEnum.filter([1], even_fun)
 IO.inspect MyEnum.filter([2, 4, 6], even_fun)
 IO.inspect MyEnum.filter([2, 3, 4], even_fun)
+IO.puts "---\n"
+
+IO.puts "MyEnum.split"
+IO.puts "---"
+IO.inspect MyEnum.split([], 2)
+IO.inspect MyEnum.split([1], 2)
+IO.inspect MyEnum.split([1, 2, 3], 2)
+IO.inspect MyEnum.split([2, 3, 4], -1)
+IO.inspect MyEnum.split([4, 6, 8], -5)
 IO.puts "---\n"
