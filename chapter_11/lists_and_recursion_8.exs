719d0edbac40027e37313ebd77b30424e9f8cf6c
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-11-18 23:23:08 -0200

    Solve lists and recursion exercise no. 8

---
+++ chapter_11/lists_and_recursion_8.exs
@@ -0,0 +1,44 @@
+defmodule SalesCalculator do
+  @tax_rates [
+    NC: 0.075,
+    TX: 0.08
+  ]
+
+  def calculate(orders) do
+    for order <- orders do
+      ship_to = @tax_rates[order[:ship_to]]
+
+      if is_nil(ship_to) do
+        append_total_amount(order, order[:net_amount])
+      else
+        apply_tax(order, order[:ship_to])
+      end
+    end
+  end
+
+  defp apply_tax(order, ship_to) do
+    net_amount = order[:net_amount]
+    total_amount = net_amount + (net_amount * @tax_rates[ship_to])
+
+    append_total_amount(order, total_amount)
+  end
+
+  def append_total_amount(order, total_amount) do
+    order ++ [
+      total_amount: total_amount
+    ]
+  end
+end
+
+orders = [
+  [id: 123, ship_to: :NC, net_amount: 100.00],
+  [id: 124, ship_to: :OK, net_amount: 35.50],
+  [id: 125, ship_to: :TX, net_amount: 24.00],
+  [id: 126, ship_to: :TX, net_amount: 44.80],
+  [id: 127, ship_to: :NC, net_amount: 25.00],
+  [id: 128, ship_to: :MA, net_amount: 10.00],
+  [id: 129, ship_to: :CA, net_amount: 102.00],
+  [id: 130, ship_to: :NC, net_amount: 50.00]
+]
+
+IO.inspect SalesCalculator.calculate(orders)
